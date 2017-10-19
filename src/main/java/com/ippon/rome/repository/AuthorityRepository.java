e0e8ea423f0d99f7627af627b397c42a8213689a
Julian Slaughter <julians@maxxpotential.com>
2017-10-19 14:14:45 -0400

    Initial JHipster Project Commit

---
+++ src/main/java/com/ippon/rome/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.ippon.rome.repository;
+
+import com.ippon.rome.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
---
+++ src/main/java/com/ippon/rome/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.ippon.rome.service.dto;
