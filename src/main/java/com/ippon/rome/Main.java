9228eb4509a4ff57f2ff3e29cac1ea6cbbe23d60
Quark Wei <quark.j.wei@gmail.com>
2017-10-31 18:41:15 -0400

    UI updates for sharing

--- src/main/java/com/ippon/rome/Main.java
+++ src/main/java/com/ippon/rome/Main.java
@@ -1,6 +1,5 @@
 package com.ippon.rome;
 
-import com.sun.glass.ui.Window;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -17,26 +16,50 @@ import java.nio.file.StandardCopyOption;
 
 public class Main extends Application {
 
-    private final TableView<Reference> table = new TableView<>();
-    private final ObservableList<Reference> data
+    private final ObservableList<Reference> myFiles
+            = FXCollections.observableArrayList();
+    private final ObservableList<Reference> sharedFiles
             = FXCollections.observableArrayList();
 
     private BorderPane borderPane;
 
-    private TableView fileList;
+    private TableView myFileList;
+    private TableView sharedFileList;
 
-    private TableColumn hashColumn, downloadColumn;
+    private TableColumn myHashColumn, myDownloadColumn, myShareColumn, sharedHashColumn, sharedDownloadColumn;
 
 
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        data.addAll(Reference.getIndex());
+        myFiles.addAll(Reference.getIndex());
+        sharedFiles.addAll(Reference.getIndex());
         primaryStage.setWidth(800);
         primaryStage.setHeight(600);
 
-        hashColumn = new TableColumn("Hash");
-        hashColumn.setCellFactory(p ->
+        myHashColumn = new TableColumn("Hash");
+        myHashColumn.setCellFactory(p ->
+                new TableCell<Reference, String>() {
+                    TextField txt = new TextField();
+                    @Override
+                    protected void updateItem(String item, boolean empty) {
+
+                        super.updateItem(item, empty);
+                        if(!empty) {
+                            Reference ref = getTableView().getItems().get(getIndex());
+                            txt.setText(ref.getHash());
+                            txt.setEditable(false);
+                            txt.setFocusTraversable(false);
+                            txt.setBackground(Background.EMPTY);
+                            setGraphic(txt);
+                        } else {
+                            setText(null);
+                        }
+
+                    }
+                });
+        sharedHashColumn = new TableColumn("Hash");
+        sharedHashColumn.setCellFactory(p ->
                 new TableCell<Reference, String>() {
                     TextField txt = new TextField();
                     @Override
@@ -57,10 +80,47 @@ public class Main extends Application {
                     }
                 });
 
-        downloadColumn = new TableColumn("Download And Decrypt");
-        downloadColumn.setMaxWidth(200.0);
-        downloadColumn.setMinWidth(200.0);
-        downloadColumn.setCellFactory(p ->
+        myDownloadColumn = new TableColumn("Download And Decrypt");
+        myDownloadColumn.setMaxWidth(200.0);
+        myDownloadColumn.setMinWidth(200.0);
+        myDownloadColumn.setCellFactory(p ->
+                new TableCell<Reference, String>() {
+                    final Button btn = new Button("Just Do It");
+                    @Override
+                    protected void updateItem(String item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if(!empty) {
+                            btn.setOnAction(event -> {
+                                Reference ref = getTableView().getItems().get(getIndex());
+                                FileChooser fileChooser = new FileChooser();
+                                fileChooser.setTitle("Choose Decrypted File Location");
+                                File targetFile = fileChooser.showSaveDialog(primaryStage);
+                                if(targetFile != null){
+                                    try {
+                                        OutputStream outputStream = new FileOutputStream(targetFile);
+                                        InputStream inputStream = ref.getData();
+                                        java.nio.file.Files.copy(
+                                                inputStream,
+                                                targetFile.toPath(),
+                                                StandardCopyOption.REPLACE_EXISTING);
+                                        outputStream.close();
+                                        inputStream.close();
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+
+                                }
+                            });
+                            setGraphic(btn);
+                            setAlignment(Pos.BASELINE_CENTER);
+                            setText(null);
+                        }
+                    }
+                });
+        sharedDownloadColumn = new TableColumn("Download And Decrypt");
+        sharedDownloadColumn.setMaxWidth(200.0);
+        sharedDownloadColumn.setMinWidth(200.0);
+        sharedDownloadColumn.setCellFactory(p ->
                 new TableCell<Reference, String>() {
                     final Button btn = new Button("Just Do It");
                     @Override
@@ -95,12 +155,42 @@ public class Main extends Application {
                     }
                 });
 
-        fileList = new TableView();
-        fileList.setItems(data);
-        fileList.getColumns().addAll(hashColumn,downloadColumn);
-        fileList.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+        myShareColumn = new TableColumn("Share File");
+        myShareColumn.setMaxWidth(200.0);
+        myShareColumn.setMinWidth(200.0);
+        myShareColumn.setCellFactory(p ->
+                new TableCell<Reference, String>() {
+                    final Button btn = new Button("Share File");
+                    @Override
+                    protected void updateItem(String item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if(!empty) {
+                            btn.setOnAction(event -> {
+                                Reference ref = getTableView().getItems().get(getIndex());
+                                //TODO prompt for public key of sharee
+                                //TODO encrypt decryption key and publish to HyperLedger
+
+                            });
+                            setGraphic(btn);
+                            setAlignment(Pos.BASELINE_CENTER);
+                            setText(null);
+                        }
+                    }
+                });
+
+        myFileList = new TableView();
+        myFileList.setItems(myFiles);
+        myFileList.getColumns().addAll(myHashColumn, myDownloadColumn, myShareColumn);
+        myFileList.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+        sharedFileList = new TableView();
+        sharedFileList.setItems(sharedFiles);
+        sharedFileList.getColumns().addAll(sharedHashColumn, sharedDownloadColumn);
+        sharedFileList.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
         borderPane = new BorderPane();
-        borderPane.setCenter(fileList);
+
+        borderPane.setTop(sharedFileList);
+        borderPane.setCenter(myFileList);
 
         Button addFileButton = new Button("Add File");
         addFileButton.setOnAction(event -> {
@@ -110,7 +200,7 @@ public class Main extends Application {
             if(selectedFile != null) {
                 try {
                     BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(selectedFile));
-                    data.addAll(new Reference(inputStream));
+                    myFiles.addAll(new Reference(inputStream));
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
